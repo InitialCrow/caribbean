bfed18c03380b22f1de4fdb59872ae5950d709e9
Adam <parent.adam@free.fr>
2016-03-29 16:09:13 +0200

    upgrade css

--- resources/views/front/temoignage.blade.php
+++ resources/views/front/temoignage.blade.php
@@ -2,25 +2,37 @@
 
 @section('content')
     @parent
-    <section class="temoignage wrapper">
-        <h1>Temoignages</h1>
-        <section class="perso-temoignage">
-            <span>Catégorie : </span>
-            <i>Icone</i>
-            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <section class="temoignage wrapper Futura_Std_Medium">
+        <h1 class="didotLTStd-Bold" >LIVRE<span class="edwardianScriptITC under">d'Or</span></h1>
+        <section class="temoignage">
+            <h2>Marine <span class="cap">Haudet</span></h2>
+            <span class="date">écrit le 14/10/15</span>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
         </section>
-        <section class="perso-temoignage">
-            <span>Catégorie : </span>
-            <i>Icone</i>
-            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <section class="temoignage right Futura_Std_Medium">
+            <h2>Victoria <span class="cap">Arella</span></h2>
+            <span class="date">écrit le 14/10/15</span>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <img src="{{url('assets/images/ex1.jpg')}}" alt="impage de temoingnage">
         </section>
-        <section class="perso-temoignage">
-            <span>Catégorie : </span>
-            <i>Icone</i>
-            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <section class="temoignage Futura_Std_Medium">
+            <h2>Pierre<span class="cap"> Garraux</span></h2>
+            <span class="date">écrit le 14/10/15</span>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
         </section>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <input type="submit" value="Envoyer"/>
+        <section class="temoignage right Futura_Std_Medium">
+            <h2>Corinne<span class="cap"> Farlme</span></h2>
+            <span class="date">écrit le 14/10/15</span>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </section>
+        <section class="temoignage Futura_Std_Medium">
+            <h2>Mark<span class="cap"> Slamo</span></h2>
+            <span class="date">écrit le 14/10/15</span>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <img src="{{url('assets/images/ex2.jpg')}}" alt="impage de temoingnage">
+        </section>
+        <button type="" class="edwardianScriptITC" >Plus de commentaire</button>
     </section>
 @stop
 @section('content')
