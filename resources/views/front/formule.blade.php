f53286fdbb00b8a3bc17e4dbcd755d80189d817a
Masson Marine <marine.mas9@gmail.com>
2016-03-29 16:07:02 +0200

    css 0.2

--- resources/views/front/formule.blade.php
+++ resources/views/front/formule.blade.php
@@ -2,8 +2,71 @@
 
 @section('content')
     @parent
-    <section class="formule wrapper">
-        <p class="title"><span class="edwardianScriptITC equipe">Nos</span> <span class="didotLTStd-Bold pour-vous">prestations</span></p>
+    <section id="formule">
+        <section class="formule wrapper">
+            <h2 class="title"><span class="edwardianScriptITC nos">Nos</span> <span class="didotLTStd-Bold prestations">prestations</span></h2>
+            <section>
+                <div class="col-md-6">
+                    <div class="pack formule-01">
+                        <div>
+                            <h3><span class="formule edwardianScriptITC">Formule</span> <span class="name">ANTHURIUM</span></h3>
+                            <p class="futuraStd-Light">Nos forfaits de consulting/conseil</p>
+                            <button class="didotLTStd-Bold">Découvrir</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="pack formule-02">
+                        <div>
+                            <h3><span class="formule edwardianScriptITC">Formule</span> <span class="name">ALPINIA</span></h3>
+                            <p class="futuraStd-Light">Nos forfaits de cérémonie</p>
+                            <button class="didotLTStd-Bold">Découvrir</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="pack formule-03">
+                        <div>
+                            <h3><span class="formule edwardianScriptITC">Formule</span> <span class="name">HIBISCUS</span></h3>
+                            <p class="futuraStd-Light">Conciergerie privée pour vous et vos convives</p>
+                            <button class="didotLTStd-Bold">Découvrir</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="pack formule-04">
+                        <div>
+                            <h3><span class="formule edwardianScriptITC">Formule</span> <span class="name">ALLAMANDA</span></h3>
+                            <p class="futuraStd-Light">Nos forfaits d’organisation</p>
+                            <button class="didotLTStd-Bold">Découvrir</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="engagement">
+                <h3><span class="edwardianScriptITC notre">Notre</span> <span class="didotLTStd-Bold engagement">ENGAGEMENT</span></h3>
+                <p class="futuraStd-Light">Une équipe de professionnels qui organise pour vous et avec vous votre événement.
+                    Nous mettons à votre disposition notre savoir faire, nos valeurs basées sur le service
+                    de qualité lié à la convivialité.
+                </p>
+                <div class="clearfix"></div>
+                <span class="border"></span>
+            </section>
+            <section class="fonctionnement">
+                <div class="txt-right">
+                    <h3><span class="edwardianScriptITC notre">Notre</span> <span class="didotLTStd-Bold fonctionnement">FONCTIONNEMENT</span></h3>
+                    <p class="futuraStd-Light"><span class="pink">Nous sommes au cœur de la relation entre vous et les prestataires</span>
+                        En fonction du niveau de prestation que vous souhaitez, nous vous proposons des fournisseurs adaptés
+                        Nous vous soumettons un devis global en portant à votre connaissance les montants par pôle d’activité
+                        Après modification de la proposition, nous procédons à la signature entre Caribbean Planner et Vous</p>
+                    <div class="clearfix"></div>
+                    <button class="edwardianScriptITC">Lire la suite</button>
+                    <div class="clearfix"></div>
+                    <span class="border"></span>
+                </div>
+            </section>
+        </section>
     </section>
 @stop
 @section('content')
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,7 +5,6 @@
         <li><a href="{{url('/formule')}}">Nos Prestations</a></li>
         <li><a href="{{url('/realisation')}}">Réalisations</a></li>
         <li><a href="{{url('/temoignage')}}">Livre d'or</a></li>
-        <li><a href="{{url('/conciergerie')}}">Conciègerie</a></li>
         <li><a href="{{url('/evenement')}}">Mon Evènement</a></li>
         <li><a href="{{url('/contact')}}">Contact</a></li>
     </ul>
